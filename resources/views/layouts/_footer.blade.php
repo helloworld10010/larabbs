aeeb95dd84a539c7a774a41307bba892758e0889
Kwok <guojam@outlook.com>
2018-03-23 10:01:35 +0000

    finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/3608114311/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1">Kwok</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="#">联系我们</a></p>
+    </div>
+</footer>
