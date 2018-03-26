af8a55a40a32b2defba9e7705031a4b66cdbe3d6
Kwok <guojam@outlook.com>
2018-03-26 02:35:24 +0000

    only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,15 +17,21 @@ use App\Handlers\ImageUploadHandler;
  */
 class UsersController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth',['except' => ['show']]);
+    }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $upload,User $user){
+        $this->authorize('update',$user);
         // 把全部数据取出来，方便验证头像有效性
         $data = $request->all();
         if($request->avatar){
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -9,11 +11,13 @@ class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
+     * 该属性用于将各种模型对应到管理它们的授权策略上。我们需要为用户模型 User 指定授权策略 UserPolicy。
      *
      * @var array
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
     ];
 
     /**
