1029721604184d5f2527b13a4e1d49fdb8a2bc9b
Kwok <guojam@outlook.com>
2018-03-31 03:25:48 +0000

    seeding topics and user data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 /*
@@ -10,14 +11,21 @@ use Faker\Generator as Faker;
 | This directory should contain each of the model factory definitions for
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
+    Carbon 是 PHP DateTime 的一个简单扩展，这里我们使用 now() 和 toDateTimeString() 来创建格式如 2017-10-13 18:42:40 的时间戳。
 |
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+    static $password;
+    $now = Carbon::now()->toDateTimeString();
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $password ?: $password = bcrypt('password'),
         'remember_token' => str_random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
