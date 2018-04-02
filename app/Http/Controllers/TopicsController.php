e1765bb543c9a12dc36de27491a30ec0266cb557
Kwok <guojam@outlook.com>
2018-04-02 01:23:03 +0000

    topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,54 +7,49 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 
-class TopicsController extends Controller
-{
-    public function __construct()
-    {
+class TopicsController extends Controller {
+    public function __construct() {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-		$topics = Topic::paginate();
-		return view('topics.index', compact('topics'));
-	}
+    public function index() {
+        /*
+         * 方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。
+         * 后面即使是在遍历数据时使用到这两个关联属性，数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询：
+         */
+        $topics = Topic::with('user','category')->paginate(20);
+        return view('topics.index', compact('topics'));
+    }
 
-    public function show(Topic $topic)
-    {
+    public function show(Topic $topic) {
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
-		return view('topics.create_and_edit', compact('topic'));
-	}
+    public function create(Topic $topic) {
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+    public function store(TopicRequest $request) {
+        $topic = Topic::create($request->all());
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
 
-	public function edit(Topic $topic)
-	{
+    public function edit(Topic $topic) {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    public function update(TopicRequest $request, Topic $topic) {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+    }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic) {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
 }
--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,17 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /*
+     * 有了以上的关联设定，后面开发中我们可以很方便地通过 $topic->category、$topic->user 来获取到话题对应的分类和作者
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
